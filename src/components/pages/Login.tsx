b31dbc1e15a43d916b764825797317478e357d21
yuichi04 <sparklingengineer.yuya04gen@gmail.com>
2023-01-16 00:14:14 +0900

    create top page

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,6 +1,8 @@
 import { FC } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
+import { PageTitle } from "../atoms";
+import { Container } from "../layouts";
 
 type LoginState = {
   email: string;
@@ -10,55 +12,64 @@ type LoginState = {
 const Login: FC = () => {
   const {
     register,
-    reset,
     handleSubmit,
     formState: { errors, isSubmitting },
   } = useForm<LoginState>();
 
   const onSubmit: SubmitHandler<LoginState> = (data) => {
     if (isSubmitting) return;
-    alert("success");
-    reset();
+    // firebaseに認証情報を問い合わせる処理
+    // ホーム画面に遷移させる処理
+    console.log(data);
   };
 
   return (
-    <div className="mx-auto p-8 bg-gray-200 font-mono w-full h-screen md:w-1/2 md:h-auto md:my-8 md:shadow-2xl">
-      <h1 className="text-3xl uppercase mb-4 text-center">login</h1>
-      <form onSubmit={handleSubmit(onSubmit)} className="flex-col uppercase mb-8">
+    <div className="bg-gray-100 p-8 shadow-lg h-screen">
+      <Container>
         <div className="mb-4">
-          <p className="text-red-600">{errors.email?.message}</p>
-          <label>メールアドレス</label>
-          <input
-            type="email"
-            className="w-full"
-            autoComplete="current-email"
-            {...register("email", {
-              required: "メールアドレスを入力してください",
-              pattern: {
-                value: /^[a-zA-Z0-9_+-]+(.[a-zA-Z0-9_+]+)*@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$/,
-                message: "正しいメールアドレスを入力してください",
-              },
-            })}
-          />
+          <PageTitle>login</PageTitle>
         </div>
-        <div className="mb-8">
-          <p className="text-red-600">{errors.password?.message}</p>
-          <label>パスワード</label>
-          <input
-            type="password"
-            className="w-full"
-            autoComplete="current-password"
-            {...register("password", {
-              required: "パスワードが入力されていません",
-              minLength: { value: 8, message: "正しいパスワードを入力してください" },
-            })}
-          />
-        </div>
-        <button className="bg-blue-800 text-white text-center w-full py-1 uppercase">Login</button>
-      </form>
-      <Link to="/signup" className="bg-stone-700 text-center text-white w-full py-1 inline-block">
-        アカウント作成はこちら
-      </Link>
+        <form onSubmit={handleSubmit(onSubmit)} className="flex-col uppercase mb-8 font-mono">
+          <div className="mb-4">
+            <p className="text-red-600">{errors.email?.message}</p>
+            <label>email:</label>
+            <input
+              type="email"
+              className="w-full"
+              autoComplete="current-email"
+              {...register("email", {
+                required: "メールアドレスを入力してください",
+                pattern: {
+                  value: /^[a-zA-Z0-9_+-]+(.[a-zA-Z0-9_+]+)*@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$/,
+                  message: "正しいメールアドレスを入力してください",
+                },
+              })}
+            />
+          </div>
+          <div className="mb-8">
+            <p className="text-red-600">{errors.password?.message}</p>
+            <label>password:</label>
+            <input
+              type="password"
+              className="w-full"
+              autoComplete="current-password"
+              {...register("password", {
+                required: "パスワードが入力されていません",
+                minLength: { value: 8, message: "正しいパスワードを入力してください" },
+              })}
+            />
+          </div>
+          <button className="bg-blue-800 text-white text-center w-full py-1 uppercase transition-opacity hover:opacity-80">
+            Login
+          </button>
+        </form>
+        <Link
+          to="/signup"
+          className="bg-stone-700 text-center text-white w-full py-1 inline-block transition-opacity hover:opacity-80"
+        >
+          アカウント作成はこちら
+        </Link>
+      </Container>
     </div>
   );
 };
--- src/components/pages/index.ts
+++ src/components/pages/index.ts
@@ -1,2 +1,3 @@
+export { default as Top } from "./Top";
 export { default as SignUp } from "./SignUp";
 export { default as Login } from "./Login";
